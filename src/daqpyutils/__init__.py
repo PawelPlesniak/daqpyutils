f8ed4f462d6d4fe9b2881677dec7f7f0ef8e2fc8
PawelPlesniak <plesniakpaul@gmail.com>
2026-07-02 18:30:31 +0200

    WIP ish

---
+++ src/daqpyutils/__init__.py
@@ -0,0 +1,3 @@
+from daqpytools.logging.logger import setup_root_logger
+
+setup_root_logger(logger_name="daqpyutils", log_level="ERROR")
---
+++ src/daqpyutils/repository_handling/__init__.py
@@ -0,0 +1,3 @@
+from daqpytools.logging.logger import get_daq_logger
+
+get_daq_logger(logger_name="daqpyutils.repository_handling")
